30c2f582544a01e9d747c507687f606c3f0e66df
mennamohamed2976 <menna.8762004@gmail.com>
2026-06-15 13:06:20 +0300

    Update settings for static and media file handling

--- config/settings.py
+++ config/settings.py
@@ -182,7 +182,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'core.User'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 #AI MATCHING
