4b68d5f65aebfd64328b1459af8bf532d5db884c
mennamohamed2976 <menna.8762004@gmail.com>
2026-06-15 13:15:30 +0300

    Remove AUTH_USER_MODEL setting from settings.py

--- config/settings.py
+++ config/settings.py
@@ -188,7 +188,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'core.User'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
